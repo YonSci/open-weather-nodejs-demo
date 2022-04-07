b62ed1e40441ee013643df6cee0b00d2e811091a
Robel Equbasilassie <robiki4life@gmail.com>
2022-04-07 10:45:25 +0300

    add Heroku to run on env port

--- app.js
+++ app.js
@@ -1,20 +1,22 @@
 const express = require('express')
 
 const app = express()
+const port = process.env.PORT || 3000;
 
-const hostname = '127.0.0.1';
-const port = 3000;
-
+// Static Files
 app.use(express.static('public'))
 app.use('/css', express.static(__dirname + 'public/css'))
 
+// Templating Engine
 app.set('views', './views')
 app.set('view engine', 'ejs')
 
+// Render the index file
 app.get('', (req, res) => {
-    res.render('index', { text: 'from ejs' })
+    res.render('index')
 })
 
-app.listen(port, hostname, () => {
-    console.info(`Server running at http://${hostname}:${port}/`);
+// Listen on port 3000
+app.listen(port, () => {
+    console.info(`Listening on port ${port}/`);
 })

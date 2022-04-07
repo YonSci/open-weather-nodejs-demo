b935b4b187548d18d6dec05fb02f507bb852e1f9
Robel Equbasilassie <robiki4life@gmail.com>
2022-04-07 10:57:43 +0300

    add fix mixed contents

--- public/js/openweathermap.js
+++ public/js/openweathermap.js
@@ -14,7 +14,7 @@ const app = {
       let key = 'fc39d687bb2ddba85f930d72cc93f5ea';
       let lang = 'en';
       let units = 'metric';
-      let url = `http://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${key}&units=${units}&lang=${lang}`;
+      let url = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${key}&units=${units}&lang=${lang}`;
       //fetch the weather
       fetch(url)
         .then((resp) => {
@@ -60,7 +60,7 @@ const app = {
                 <div class="card">
                 <h5 class="card-title p-2">${dt.toDateString()}</h5>
                   <img
-                    src="http://openweathermap.org/img/wn/${
+                    src="https://openweathermap.org/img/wn/${
                       day.weather[0].icon
                     }@4x.png"
                     class="card-img-top"
